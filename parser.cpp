b42ef5618cf903d253e11abf25fcd2b18e723f20
Saotsu <washingtonfbueno@hotmail.com>
2023-05-07 19:37:53 -0300

    added other math functions and unary operators

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,4 @@
 #include <cmath>
-#include <functional>
 #include <iostream>
 #include <queue>
 #include <unordered_map>
@@ -22,18 +21,42 @@ class Parse {
         EQUAL,
         NOT_EQUAL,
         AND,
-        OR
+        OR,
+
+        SIN,
+        COS,
+        TAN,
+        ARCSIN,
+        ARCCOS,
+        ARCTAN,
+        LOG,
+        LOG2,
+        LOG10,
+        CEIL,
+        FLOOR,
+        ROUND,
+        TRUNC,
+        ABS,
+        SQRT
     };
 
-    std::unordered_map<std::string, int> to_enum = {
-        {"**", POW},   {"*", MULT},          {"/", DIV},    {"%", MOD},
-        {"+", PLUS},   {"-", MINUS},         {"<", LOWER},  {"<=", LOWER_EQUAL},
-        {">", HIGHER}, {">=", HIGHER_EQUAL}, {"==", EQUAL}, {"!=", NOT_EQUAL},
-        {"&&", AND},   {"||", OR},
-    };
+    std::unordered_map<std::string, int> binary_to_enum = {
+        {"**", POW},          {"*", MULT},         {"/", DIV},
+        {"%", MOD},           {"+", PLUS},         {"-", MINUS},
+        {"<", LOWER},         {"<=", LOWER_EQUAL}, {">", HIGHER},
+        {">=", HIGHER_EQUAL}, {"==", EQUAL},       {"!=", NOT_EQUAL},
+        {"&&", AND},          {"||", OR}};
 
-    int binary_priority(std::string binary_op) {
-        switch (to_enum[binary_op]) {
+    std::unordered_map<std::string, int> unary_to_enum = {
+        {"+", PLUS},        {"-", MINUS},       {"sin", SIN},
+        {"cos", COS},       {"tan", TAN},       {"arcsin", ARCSIN},
+        {"arccos", ARCCOS}, {"arctan", ARCTAN}, {"log", LOG},
+        {"log2", LOG2},     {"log10", LOG10},   {"ceil", CEIL},
+        {"floor", FLOOR},   {"round", ROUND},   {"trunc", TRUNC},
+        {"abs", ABS},       {"sqrt", SQRT}};
+
+    int op_priority(std::string op) {
+        switch (binary_to_enum[op]) {
             case POW:
                 return 7;
             case MULT:
@@ -66,9 +89,10 @@ class Parse {
 
         return 0;
     }
+
     // evaluate left and right operands within their operator
     double eval_binary(double left, double right, std::string binary_op) {
-        switch (to_enum[binary_op]) {
+        switch (binary_to_enum[binary_op]) {
             case POW:
                 return std::pow(left, right);
             case MULT:
@@ -102,6 +126,73 @@ class Parse {
         return 0;
     }
 
+    double eval_unary(double right, std::string unary_op) {
+        switch (unary_to_enum[unary_op]) {
+            case MINUS:
+                return -right;
+            case PLUS:
+                return right;
+            case SIN:
+                return std::sin(right * 3.14159265 / 180);
+            case COS:
+                return std::cos(right * 3.14159265 / 180);
+            case TAN:
+                return std::tan(right * 3.14159265 / 180);
+            case ARCSIN:
+                return std::asin(right) * 180 / 3.14159265;
+            case ARCCOS:
+                return std::acos(right) * 180 / 3.14159265;
+            case ARCTAN:
+                return std::atan(right) * 180 / 3.14159265;
+            case LOG:
+                return std::log(right);
+            case LOG2:
+                return std::log2(right);
+            case LOG10:
+                return std::log10(right);
+            case CEIL:
+                return std::ceil(right);
+            case FLOOR:
+                return std::floor(right);
+            case ROUND:
+                return std::round(right);
+            case TRUNC:
+                return std::trunc(right);
+            case ABS:
+                return std::abs(right);
+            case SQRT:
+                return std::sqrt(right);
+        }
+
+        return 0;
+    }
+
+    // check if the operator is right associative, meaning we should evaluate
+    // from right to left
+    bool is_right_associative(std::string op) {
+        if (op == "**") {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool is_unary(std::string token) {
+        if (unary_to_enum.count(token)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool has_lower_priority(std::string token, std::string previous_token) {
+        if (token == previous_token) {
+            return !is_right_associative(token);
+        }
+
+        return op_priority(token) <= op_priority(previous_token);
+    }
+
     double eval_primary() {
         std::string token = tokens->front();
 
@@ -117,21 +208,17 @@ class Parse {
         tokens->pop();
         return std::stod(token);
     }
-
-    // check if the operator is right associative, meaning we should evaluate
-    // from right to left
-    bool is_right_associative(std::string op) { return (op == "**"); }
-
-    bool has_lower_priority(std::string token, std::string previous_token) {
-        if (token == previous_token) {
-            return !is_right_associative(token);
-        }
-
-        return binary_priority(token) <= binary_priority(previous_token);
-    }
-
     double parse_pratt(std::string previous_token = "") {
-        double left = eval_primary();
+        double left{};
+
+        if (is_unary(tokens->front())) {
+            std::string unary_op = tokens->front();
+            tokens->pop();
+            double right = parse_pratt(tokens->front());
+            left = eval_unary(right, unary_op);
+        } else {
+            left = eval_primary();
+        }
 
         while (!tokens->empty()) {
             std::string token = tokens->front();
@@ -156,7 +243,7 @@ class Parse {
 };
 
 int main() {
-    std::queue<std::string> q({"7", "*", "4", "+", "2"});
+    std::queue<std::string> q({"sin", "0.5", "+", "2"});
     Parse expr = Parse(&q);
     std::cout << expr.result() << '\n';
 }
